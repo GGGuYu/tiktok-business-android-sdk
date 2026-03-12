2ff3931d05f730cc4fc818fb96cc558998e57eaf
hanweiwei <hanweiwei@bytedance.com>
2026-03-12 20:59:05 +0800

    unify appinfo parameters

--- business-core/src/main/java/com/tiktok/appevents/TTRequestBuilder.java
+++ business-core/src/main/java/com/tiktok/appevents/TTRequestBuilder.java
@@ -14,7 +14,6 @@ import androidx.annotation.Nullable;
 
 import com.tiktok.TikTokBusinessSdk;
 import com.tiktok.util.JSON;
-import com.tiktok.util.LastSessionUtil;
 import com.tiktok.util.SystemInfoUtil;
 import com.tiktok.util.TTUtil;
 import com.tiktok.util.TimeUtil;
@@ -176,20 +175,9 @@ class TTRequestBuilder {
     }
 
     private static JSONObject contextBuilder(@Nullable TTIdentifierFactory.AdIdInfo adIdInfo, boolean isDDL) {
-        JSONObject app = JSON.build();
+        JSONObject app = TTRequest.getAppInfo();
         try {
-            if (TikTokBusinessSdk.bothIdsProvided()) {
-                JSON.putObject(app, "id", TikTokBusinessSdk.getAppId());
-            }
-            JSON.putObject(app, "name", SystemInfoUtil.getAppName());
-            JSON.putObject(app, "namespace", SystemInfoUtil.getPackageName());
-            JSON.putObject(app, "version", SystemInfoUtil.getAppVersionName());
-            JSON.putObject(app, "build", SystemInfoUtil.getAppVersionCode() + "");
-            JSON.putObject(app, "tiktok_app_id", TikTokBusinessSdk.getTTAppId());
-            JSON.putObject(app, "app_session_id", SystemInfoUtil.getAppSessionId());
             JSON.putObject(app, "anonymous_id", TTUserInfo.sharedInstance.anonymousId);
-
-            LastSessionUtil.inject2RequestParam(app);
         } catch (Throwable ignore) {
         }
 
